334e15bf6dccb6708fe2ebd3401c7226e109f393
J-zinu <wjswlsdn4053@naver.com>
2024-07-15 18:36:45 +0900

    feat: DimScreenSample.swift에서 polygon은 구현했으나, MapPolygon은 구현되는지 모르겠음...

--- KakaoMapsSDK-Sample/Samples/Polygon/DimScreenSample.swift
+++ KakaoMapsSDK-Sample/Samples/Polygon/DimScreenSample.swift
@@ -29,8 +29,8 @@ class DimScreenSample: APISampleBaseViewController {
         let mapView: KakaoMap = mapController?.getView("mapview") as! KakaoMap
         let dimScreen: DimScreen = mapView.dimScreen
         let shapeStyle = PolygonStyle(styles: [
-            PerLevelPolygonStyle(color: UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 0.5), strokeWidth: 2, strokeColor: UIColor.blue, level: 0),
-            PerLevelPolygonStyle(color: UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 0.5), strokeWidth: 2, strokeColor: UIColor.red, level: 15)
+            PerLevelPolygonStyle(color: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0), strokeWidth: 2, strokeColor: UIColor.blue, level: 0),
+            PerLevelPolygonStyle(color: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0), strokeWidth: 2, strokeColor: UIColor.red, level: 15)
         ])
         let shapeStyleSet = PolygonStyleSet(styleSetID: "shapeLevelStyle1", styles: [shapeStyle])
         dimScreen.addPolygonStyleSet(shapeStyleSet)
@@ -38,30 +38,34 @@ class DimScreenSample: APISampleBaseViewController {
         let viewSize = mapView.viewRect.size
         let options = PolygonShapeOptions(shapeID: "shape1", styleID: "shapeLevelStyle1", zOrder: 1)
         options.basePosition = mapView.getPosition(CGPoint(x: viewSize.width/2, y: viewSize.height/2))
-        let outter = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 1.0, numPoints: 90, cw: true), hole: Primitives.getCirclePoints(radius: 0.8, numPoints: 90, cw: false), styleIndex: 0)
-        let mid = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 0.6, numPoints: 90, cw: true), hole: Primitives.getCirclePoints(radius: 0.4, numPoints: 90, cw: false), styleIndex: 0)
-        let inner = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 0.2, numPoints: 90, cw: true), hole: nil, styleIndex: 0)
+        
+        let outter = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 1000.0, numPoints: 90, cw: true), hole: nil, styleIndex: 0)
+//        let outter = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 1.0, numPoints: 90, cw: true), hole: Primitives.getCirclePoints(radius: 0.8, numPoints: 90, cw: false), styleIndex: 0)
+        
+        
+//        let mid = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 0.6, numPoints: 90, cw: true), hole: Primitives.getCirclePoints(radius: 0.4, numPoints: 90, cw: false), styleIndex: 0)
+//        let inner = Polygon(exteriorRing: Primitives.getCirclePoints(radius: 0.2, numPoints: 90, cw: true), hole: nil, styleIndex: 0)
         
         options.polygons.append(outter)
-        options.polygons.append(mid)
-        options.polygons.append(inner)
+//        options.polygons.append(mid)
+//        options.polygons.append(inner)
         
         let shape = dimScreen.addHighlightPolygonShape(options)
         shape?.show()
         
         let effect = WaveAnimationEffect(datas: [
-            WaveAnimationData(startAlpha: 0.8, endAlpha: 0.0, startRadius: 10.0, endRadius: 100.0, level: 0),
-            WaveAnimationData(startAlpha: 0.8, endAlpha: 0.0, startRadius: 50, endRadius: 500, level: 15)
+            WaveAnimationData(startAlpha: 0.8, endAlpha: 0.0, startRadius: 50000, endRadius: 50000, level: 0),
+            WaveAnimationData(startAlpha: 0.8, endAlpha: 0.0, startRadius: 50000, endRadius: 50000, level: 15)
         ])
-        effect.hideAtStop = true
-        effect.interpolation = AnimationInterpolation(duration: 1500, method: .cubicOut)
-        effect.playCount = 15
+//        effect.hideAtStop = true
+//        effect.interpolation = AnimationInterpolation(duration: 1500, method: .cubicOut)
+//        effect.playCount = 15
         
-        let animator = dimScreen.addShapeAnimator(animatorID: "circleWave", effect: effect)
+//        let animator = dimScreen.addShapeAnimator(animatorID: "circleWave", effect: effect)
         
         // animaotr 추가.
-        animator?.addPolygonShape(shape!)
-        animator?.start()
+//        animator?.addPolygonShape(shape!)
+//        animator?.start()
     }
     
     @IBAction func dimScreenSwitchChanged(_ sender: Any) {
